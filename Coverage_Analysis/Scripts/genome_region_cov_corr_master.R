e4dbf943f177bf0502cebe01a9dafd33af5a6b22
a-karma <76513982+a-karma@users.noreply.github.com>
2022-04-02 15:40:10 +0100

    Update genome_region_cov_corr_master.R

--- Coverage_Analysis/Scripts/genome_region_cov_corr_master.R
+++ Coverage_Analysis/Scripts/genome_region_cov_corr_master.R
@@ -1,32 +1,32 @@
 
 ################################################## Defining input and vars ############################################
 
-region_df<-"../Output/region_1_table.csv"
+region_df<-"./Output/region_1_table.csv"
 region="Region1 (80 bp)"
-source("./genome_region_cov_corr_plot.R")
+source("./Scripts/genome_region_cov_corr_plot.R")
 ################################################## Defining input and vars ############################################
 
-region_df<-"../Output/region_2_table.csv"
+region_df<-"./Output/region_2_table.csv"
 region="Region2 (205 bp)"
-source("./genome_region_cov_corr_plot.R")
+source("./Scripts/genome_region_cov_corr_plot.R")
 
 ################################################## Defining input and vars ############################################
 
-region_df<-"../Output/region_3_table.csv"
+region_df<-"./Output/region_3_table.csv"
 region="Region3 (245 bp)"
-source("./genome_region_cov_corr_plot.R")
+source("./Scripts/genome_region_cov_corr_plot.R")
 
 ################################################## Defining input and vars ############################################
 
-region_df<-"../Output/region_4_table.csv"
+region_df<-"./Output/region_4_table.csv"
 region="Region4 (300 bp)"
-source("./genome_region_cov_corr_plot.R")
+source("./Scripts/genome_region_cov_corr_plot.R")
 
 
 ################################################## Defining input and vars ############################################
 
-region_df<-"../Output/region_5_table.csv"
+region_df<-"./Output/region_5_table.csv"
 region="Region5 (230 bp)"
-source("./genome_region_cov_corr_plot.R")
+source("./Scripts/genome_region_cov_corr_plot.R")
 
 #######################################################################################################################
