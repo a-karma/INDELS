5e44fabd79cf20a511d0ab3564614166176996da
a-karma <76513982+a-karma@users.noreply.github.com>
2022-04-02 10:13:39 +0100

    Add files via upload

---
+++ Coverage_Analysis/Scripts/genome_region_cov_corr_plot.R
@@ -0,0 +1,28 @@
+library(ggplot2)
+library(ggrepel)
+
+tab<-read.table(region_df, header = T, sep="\t",dec = ".")
+colnames(tab)<-c("ID","Region","Length","Mean_Cov_Depth","Breadth","Genome_Depth","Type")
+
+dat<-data.frame(tab$ID,tab$Mean_Cov_Depth,tab$Genome_Depth,tab$Type)
+names(dat)<-c("ID","R_doc","G_doc","Type")
+x_label="Mean genome Depth of Coverage"
+y_label="Mean region Depth of Coverage"
+
+col<-c("#1A80C4","#CC3D3D")
+
+pdf(paste(levels(tab$Region),levels(as.factor(tab$Length)),"bp_corr_plot.pdf", sep="_", collapse=NULL))
+print(
+ggplot(dat,aes(G_doc,R_doc))+
+  geom_point(aes(colour = Type), size=3, alpha=0.7) +
+  labs(x=x_label , y=y_label)+
+  scale_colour_manual(values=col) +
+  geom_abline(intercept=0, slope=1)+
+  geom_abline(intercept=0, slope=2, linetype="dashed")+
+  geom_abline(intercept=0, slope=4, linetype="dashed")+
+  geom_abline(intercept=0, slope=0.5, linetype="dashed")+
+  geom_abline(intercept=0, slope=0.25, linetype="dashed")+
+  geom_label_repel(aes(label = ID), box.padding = 0.9, point.padding=0.2, size=2.5)+
+  ggtitle(paste("Coverage Depth correlation plot: ",region)))
+dev.off()
+
---
+++ Coverage_Analysis/Scripts/heat_map_cov_plot.R
@@ -0,0 +1,45 @@
+setwd("Desktop/Willis_Synd/final/")
+
+All_region_df<-"edited_coverage_database.csv"
+
+library(ggplot2)
+library(reshape)
+library(scales)
+library(wesanderson)
+library(ggtext)
+library(tidyverse)
+
+tab<-read.table(All_region_df, header = T, sep="\t",dec = ".")
+dat1<-data.frame(tab$Sample_ID,tab$Region,tab$E.DoC.,tab$Type)
+names(dat1)<-c("ID","Region","Mean_Coverage","Type")
+pal <- wes_palette("Zissou1", 21, type = "continuous")
+
+for (i in seq(length(dat1$Mean_Coverage))){
+if ( dat1$Mean_Coverage[i] > 20 ){
+  dat1$Mean_Coverage[i]=20
+}}
+
+pdf(paste("heat_map_mean_doc.pdf", sep="", collapse=NULL), width=7, height=21, pointsize=10)
+print(
+  ggplot(dat1, aes(x = Region, y = ID, fill = Mean_Coverage)) +
+        geom_tile() +
+        scale_fill_gradientn(colours = pal ,values = rescale(c(0,0.01,0.1,0.25,0.5,1))) +
+        theme(axis.text = element_text(size=12), axis.title = element_blank())+
+        scale_x_discrete(expand = c(0, 0)) +
+        scale_y_discrete(expand = c(0, 0)))
+dev.off()
+
+
+
+dat2<-data.frame(tab$Sample_ID,tab$Region,tab$BoC)
+names(dat2)<-c("ID","Region","Breadth")
+
+pdf(paste("heat_map_boc.pdf", sep="", collapse=NULL), width=7, height=21, pointsize=10)
+print(
+  ggplot(dat2, aes(x = Region, y = ID, fill =Breadth)) +
+    geom_tile() +
+    scale_fill_gradientn(colours = pal ,values = rescale(c(0,0.01,0.1,0.2,0.4,1))) +
+    theme(axis.text = element_text(size=12), axis.title = element_blank())+
+    scale_x_discrete(expand = c(0, 0)) +
+    scale_y_discrete(expand = c(0, 0)))
+dev.off()
